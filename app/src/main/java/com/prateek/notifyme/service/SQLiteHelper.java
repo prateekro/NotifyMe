63810ab9e274688319714f75d78f09a35f5bde59
Akash Gupta <agupta6@scu.edu>
2019-02-26 08:06:28 -0800

    SQLite helper made general for all entities

--- app/src/main/java/com/prateek/notifyme/service/SQLiteHelper.java
+++ app/src/main/java/com/prateek/notifyme/service/SQLiteHelper.java
@@ -11,12 +11,17 @@ import com.prateek.notifyme.beans.Notification;
 
 public class SQLiteHelper extends SQLiteOpenHelper {
     private static final String TAG = "DatabaseHelper";
-    private static final String TABLE_NAME = "notification_table";
-    private static final String COL1 = "ID";
-    private static final String COL2 = "TIMESTAMP";
-    private static final String COL3 = "APPNAME";
-    private static final String COL4 = "TEXT";
-    private static final String COL5 = "PRIORITY";
+    private static final String NOTIFICATION_TABLE_NAME = "notification_table";
+    private static final String NOTIFICATION_COL1 = "ID";
+    private static final String NOTIFICATION_COL2 = "TIMESTAMP";
+    private static final String NOTIFICATION_COL3 = "APPNAME";
+    private static final String NOTIFICATION_COL4 = "TEXT";
+    private static final String NOTIFICATION_COL5 = "PRIORITY";
+
+    private static final String USER_TABLE_NAME = "user_table";
+    private static final String USER_COL1 = "EMAIL";
+    private static final String USER_COL2 = "NAME";
+    private static final String USER_COL3 = "PASSWORD";
 
     public SQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -24,20 +29,28 @@ public class SQLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (" +
-                COL1+ "TEXT PRIMARY KEY, "+
-                COL2+ "DATETIME NOT NULL, "+
-                COL3+ "TEXT NOT NULL, "+
-                COL4+ "TEXT NOT NULL, "+
-                COL5+ "TEXT NOT NULL); ";
-        db.execSQL(createTable);
+        String createTable_notification = "CREATE TABLE " + NOTIFICATION_TABLE_NAME + " (" +
+                NOTIFICATION_COL1+ "TEXT PRIMARY KEY, "+
+                NOTIFICATION_COL2+ "DATETIME NOT NULL, "+
+                NOTIFICATION_COL3+ "TEXT NOT NULL, "+
+                NOTIFICATION_COL4+ "TEXT NOT NULL, "+
+                NOTIFICATION_COL5+ "TEXT NOT NULL); ";
+        db.execSQL(createTable_notification);
+
+        String createTable_user = "CREATE TABLE " + USER_TABLE_NAME+ " (" +
+                USER_COL1+ "TEXT PRIMARY KEY, "+
+                USER_COL2+ "DATETIME NOT NULL, "+
+                USER_COL3+ "TEXT NOT NULL); ";
+        db.execSQL(createTable_user);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+ USER_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+ NOTIFICATION_TABLE_NAME);
     }
 
 
 
 }
+    
