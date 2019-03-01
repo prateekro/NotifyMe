2803c6617179f0dde29cbf16325d18a516396a12
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-03-01 00:20:06 -0800

    Added getNotifications logic

--- app/src/main/java/com/prateek/notifyme/service/NotificationService.java
+++ app/src/main/java/com/prateek/notifyme/service/NotificationService.java
@@ -1,14 +1,20 @@
 package com.prateek.notifyme.service;
 
+import android.database.Cursor;
+
 import com.prateek.notifyme.beans.Notification;
 import com.prateek.notifyme.beans.User;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class NotificationService {
+    private SQLiteHelper mDatabaseHelper;
     //on receiving every notification, save target app, notification text, timestamp, priority, unreadCounter
-    private void saveNotification(Notification notification){
+    public void saveNotification(Notification notification){
         String id = notification.getId();
         String appName = notification.getAppName();
         Date time = notification.getTimestamp();
@@ -17,34 +23,40 @@ public class NotificationService {
 
         //TODO: add "if not on the app's notification listing page", only then update unread counter
         //check whether app name entry already exists in Application DB, if not save it, if yes update and "if not on the app's notification listing page", only then update unread counter
+
     }
 
     //retrieve all app listings along with their unread counter for dashboard page
     //private Map<Application, unreadCounter> getAppListing()
 
     //on app tap from dashboard, the unread counter should reset
-    private void resetCounter(String appName){
+    public void resetCounter(String appName){
 
     }
 
     //get all stored notifications
-    private List<Notification> getNotifications(){
-
-        return null;
+    public HashMap<String, Integer> getNotifications(){
+        Cursor data = mDatabaseHelper.getApplicationListingData();
+        HashMap<String, Integer> listData = new HashMap<String, Integer>();
+        while (data.moveToNext()) {
+            // get all names and put in list
+            listData.put(data.getString(2), data.getInt(7));
+        }
+        return listData;
     }
 
     //delete all notifications of a particular app
-    private void deleteNotification(String appName){
+    public void deleteNotification(String appName){
 
     }
 
     //delete all notifications
-    private void clearAllNotifications(){
+    public void clearAllNotifications(){
 
     }
 
     //delete a particular notification
-    private void deleteNotification(Notification notification){
+    public void deleteNotification(Notification notification){
 
     }
 }
