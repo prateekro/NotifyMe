3d8be5c1d66ce24ec8c639ab65da23f5cdf3381d
Shreyas Padhye <shreyaspadhye3011@gmail.com>
2019-03-10 01:26:10 -0800

    firebase setup and write update working

--- app/src/main/java/com/prateek/notifyme/NotificationListing.java
+++ app/src/main/java/com/prateek/notifyme/NotificationListing.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.prateek.notifyme.adapter.AppListElementAdapter;
 import com.prateek.notifyme.commons.utils;
 import com.prateek.notifyme.elements.ListElement;
@@ -32,7 +38,29 @@ public class NotificationListing extends AppCompatActivity {
 
         Toast.makeText(this, "Reached on new page with title: "+pageTitle, Toast.LENGTH_SHORT).show();
 
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("obj1");
+//        myRef.setValue("Hello, World!");
+//        myRef.setValue("Check instance");
+        myRef.child("change").setValue("true");
 
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                System.out.println("Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                System.out.println("Failed to read value."+ error.toException());
+            }
+        });
 
     }
 
